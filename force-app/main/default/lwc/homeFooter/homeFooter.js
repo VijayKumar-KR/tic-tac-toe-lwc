ee854a3a0767f945baf51fd7e45f6445d8deb49a
VijayKumar-KR <vijay.kr@rubrik.com>
2022-07-31 06:15:06 -0700

    header Logo + home page footer till social media

---
+++ force-app/main/default/lwc/homeFooter/homeFooter.js
@@ -0,0 +1,38 @@
+import { LightningElement } from 'lwc';
+// logo-Image
+import salesforce_img from '@salesforce/resourceUrl/salesforce_img'; 
+import linkedin_img from '@salesforce/resourceUrl/linkedin_img';
+import wordpress_img from '@salesforce/resourceUrl/wordpress_img';
+import insta_img from '@salesforce/resourceUrl/insta_img';
+import facebook_img from '@salesforce/resourceUrl/facebook_img';
+import twitter_img from '@salesforce/resourceUrl/twitter_img';
+// navigation
+import { NavigationMixin } from 'lightning/navigation';
+
+export default class HomeFooter extends NavigationMixin (LightningElement) {
+    
+    // socail media icons
+    iconsList = [
+                    {'dataid': "/", 'src' : salesforce_img, 'alt' : 'Salesforce'},
+                    {'dataid': "https://www.linkedin.com/in/vijaykumarkr/", 'src' : linkedin_img,   'alt' : 'LinkedIn'},
+                    {'dataid': "https://sfdcchampion4u.wordpress.com/", 'src' : wordpress_img,  'alt' : 'WordPress'},
+                    {'dataid': "https://www.instagram.com/veee__jay/", 'src' : insta_img,      'alt' : 'Instagram'},
+                    {'dataid': "https://www.facebook.com", 'src' : facebook_img,   'alt' : 'FaceBook'},
+                    {'dataid': "https://www.twitter.com", 'src' : twitter_img,    'alt' : 'Twitter'}
+                ]
+
+    // navigate to webpage
+    handleClickSocialIcon(event){
+        console.log('url: ', event.currentTarget.getAttribute("data-id"));
+        let url = event.currentTarget.getAttribute("data-id");
+        const config = {
+            type: 'standard__webPage',
+            attributes: {
+                url: url
+            }
+        };
+        this[NavigationMixin.Navigate](config);
+        
+    }
+
+}
