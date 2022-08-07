56f4f4926ecd7457aa4a044f9618c985b75ad8e4
VijayKumar-KR <vijay.kr@rubrik.com>
2022-08-07 12:25:56 +0530

    added text arrays

--- force-app/main/default/lwc/about/about.js
+++ force-app/main/default/lwc/about/about.js
@@ -2,7 +2,7 @@ import { LightningElement, api } from 'lwc';
 
 export default class About extends LightningElement {
 
-    @api textArray = ['Salesforce Developer', 'Blogger']
+    @api textArray = ['Salesforce Developer', 'Blogger', 'Enthusiastic', 'Ambitious']
 
     charIndex = 0;
     arrayIndex = 0;
@@ -23,7 +23,7 @@ export default class About extends LightningElement {
         let text = this.textArray[this.arrayIndex];
         if(text){
             this.charIndex++;
-            this.template.querySelector('[data-id=text').innerHTML = `<h1>I am a ${text.slice(0, this.charIndex)} </h1>`;
+            this.template.querySelector('[data-id=text').innerHTML = `<h1>I am ${(text.slice(0, 1) === 'E') || (text.slice(0, 1) === 'A')  ? 'an' : 'a'} ${text.slice(0, this.charIndex)} </h1>`;
             if(text.length === this.charIndex){
                 this.arrayIndex++;
                 this.charIndex = 0;
