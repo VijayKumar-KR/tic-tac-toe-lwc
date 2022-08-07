54bdacbbc40dbf3bbb60e85b4c699e9f15047dd2
VijayKumar-KR <vijay.kr@rubrik.com>
2022-08-07 08:43:39 +0530

    about page cmp

---
+++ force-app/main/default/lwc/about/about.js
@@ -0,0 +1,35 @@
+import { LightningElement, api } from 'lwc';
+
+export default class About extends LightningElement {
+
+    @api textArray = ['Salesforce Developer', 'Blogger']
+
+    charIndex = 0;
+    arrayIndex = 0;
+    timer
+
+    connectedCallback(){
+        this.timer = setInterval(() => {
+            this.displayText();
+        }, 400);
+    }
+
+    // destroy timer
+    disconnectedCallback() {
+        clearInterval(this.timer);
+    }
+
+    displayText(){
+        let text = this.textArray[this.arrayIndex];
+        if(text){
+            this.charIndex++;
+            this.template.querySelector('[data-id=text').innerHTML = `<h1>I am a ${text.slice(0, this.charIndex)} </h1>`;
+            if(text.length === this.charIndex){
+                this.arrayIndex++;
+                this.charIndex = 0;
+            }
+        } else{
+            this.arrayIndex = 0;
+        }
+    }
+}
